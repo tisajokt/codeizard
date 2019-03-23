ea6c55c57b9997a0b0b5e9834e4f2520035bb4d1
Tisajokt <james.and.tisa@gmail.com>
2019-03-23 03:59:11 -0400

    Edit heroku example to restore previous functionality

--- server.js
+++ server.js
@@ -13,27 +13,8 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-/*const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const path = require("path");
-const PORT = process.env.port || 8080;
-
-app.use("/", express.static(path.join(__dirname, "client")));
-
-server.listen(PORT, () => {
-	console.log(`Started server, listening on port ${PORT}`);
-});
-
-io.on("connection", (socket) => {
-	console.log(`Socket connected ${socket.id}`);
+	console.log('Client connected');
+	socket.on('disconnect', () => console.log('Client disconnected'));
 	socket.on("me", (data) => {
 		if (data == "app") {
 			app_socket(socket);
@@ -57,4 +38,4 @@ function web_socket(socket) {
 	socket.on("notif", (notif) => {
 		console.log(notif);
 	});
-};*/
+};
