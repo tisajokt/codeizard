ab5e7e77da40b36bb08104ef989348336ff5bda4
Tisajokt <james.and.tisa@gmail.com>
2019-03-23 03:52:22 -0400

    Replace server.js with sample heroku code

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 
-const express = require("express");
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'client/index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+/*const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
@@ -37,4 +57,4 @@ function web_socket(socket) {
 	socket.on("notif", (notif) => {
 		console.log(notif);
 	});
-};
+};*/
